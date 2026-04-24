c30a754da046f1d1672f9e95dc13d32e823d710e
lee-ching1002 <d0923252665@gmail.com>
2026-04-24 22:30:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import gspread
 import json
 
-# 讀取 Secrets
-json_str = st.secrets["GCP_JSON"]
-
-# 這一行會確保 JSON 字串內的 \n 能被真正轉換成換行符號
-creds_dict = json.loads(json_str.encode('utf-8').decode('unicode_escape'))
-
-gc = gspread.service_account_from_dict(creds_dict)
-sh = gc.open('工程科排隊系統').sheet1
+# 為了確保讀取正確，我們直接檢查 st.secrets 的內容
+# 如果您在網頁版設定的是 GCP_JSON，程式碼如下：
+if "GCP_JSON" in st.secrets:
+    json_str = st.secrets["GCP_JSON"]
+    creds_dict = json.loads(json_str)
+    gc = gspread.service_account_from_dict(creds_dict)
+    sh = gc.open('工程科排隊系統').sheet1
+else:
+    st.error("找不到 GCP_JSON 設定！請檢查 Streamlit Cloud 的 Secrets 設定。")
+    st.stop()
